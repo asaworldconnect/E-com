fa7146be2d1c4fe3d0ce926eef830211bf0044bb
Adediran Sulaiman Abiodun <Adediranabiodun424@gmail.com>
2023-08-23 18:57:34 +0100

    i change some code

---
+++ src/script.js
@@ -0,0 +1,13 @@
+const cartIcon = document.getElementById("cartIcon");
+const cartOverlay = document.getElementById("cartOverlay");
+const closeCartButton = document.getElementById("closeCart");
+
+cartIcon.addEventListener("click", () => {
+  cartOverlay.style.display = "flex";
+  document.body.style.overflow = "hidden"; // Prevent scrolling while cart is open
+});
+
+closeCartButton.addEventListener("click", () => {
+  cartOverlay.style.display = "none";
+  document.body.style.overflow = "auto"; // Restore scrolling when cart is closed
+});
