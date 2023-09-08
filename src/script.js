9fedf61a324f555f83334a85f88430837912ecb2
Adediran Sulaiman Abiodun <Adediranabiodun424@gmail.com>
2023-09-08 20:04:43 +0100

    I added some javascript to my code

--- src/script.js
+++ src/script.js
@@ -12,5 +12,40 @@ closeCartButton.addEventListener("click", () => {
   document.body.style.overflow = "auto"; // Restore scrolling when cart is closed
 });
 
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
 
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
 
+  // console.log(basket);
+  update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  // console.log(basket);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  // console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+};
